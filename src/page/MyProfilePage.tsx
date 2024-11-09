ee4bb06ef21e00dacf2d50f85e08f5e180774145
jeongmin <jeongmin010324@gmail.com>
2024-11-10 04:36:25 +0900

    feat : 색상 백엔드 연동

--- src/page/MyProfilePage.tsx
+++ src/page/MyProfilePage.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import "../asset/style/profile.css"
-import {ColorPicker} from "antd";
+import {Button, ColorPicker} from "antd";
 import {useNavigate} from "react-router-dom";
 import {useRecoilState} from "recoil";
 import menuAtom from "../recoil/menu";
+import {API} from "../util/api";
 
 
 interface IEmotion {
@@ -17,87 +18,119 @@ interface IEmotion {
   boring: string;
 }
 
-const dummy: IEmotion = {
-  joy: "6cff03",
-  angry: "ed0505",
-  sad: "ed6605",
-  afraid: "ed9805",
-  admiration: "1cff03",
-  surprise: "9eff03",
-  interest: "ddff03",
-  boring: "edbb05",
-}
 
 const MyProfilePage = () => {
   const navigate = useNavigate();
-  const [joy, setJoy] = useState<string>("#6cff03");
-  const [angry, setAngry] = useState<string>("#ed0505");
-  const [sad, setSad] = useState<string>("#ed6605");
-  const [afraid, setAfraid] = useState<string>("#ed9805");
-  const [admiration, setAdmiration] = useState<string>("#1cff03");
-  const [surprise, setSurprise] = useState<string>("#9eff03");
-  const [interest, setInterest] = useState<string>("#ddff03");
-  const [boring, setBoring] = useState<string>("#edbb05");
+  const [joy, setJoy] = useState<string>();
+  const [angry, setAngry] = useState<string>();
+  const [sad, setSad] = useState<string>();
+  const [afraid, setAfraid] = useState<string>();
+  const [admiration, setAdmiration] = useState<string>();
+  const [surprise, setSurprise] = useState<string>();
+  const [interest, setInterest] = useState<string>();
+  const [boring, setBoring] = useState<string>();
 
   const [menu, setMenu] = useRecoilState(menuAtom);
 
   useEffect(() => {
-    setMenu({id:"3"})
-
-    setJoy("#6cff03")
-    setAngry("#ed0505")
-    setSad("#ed6605")
-    setAfraid("#ed9805")
-    setAdmiration("#1cff03")
-    setSurprise("#9eff03")
-    setInterest("#ddff03")
-    setBoring("#edbb05")
+    setMenu({id: "3"})
+    getColorList()
   }, []);
+
+  const getColorList = async () => {
+    const res = await API.get("/profile/get-color");
+    const {joy, angry, sad, afraid, admiration, surprise, interest, boring} = res.data;
+    setJoy("#" + joy);
+    setAngry("#" + angry);
+    setSad("#" + sad);
+    setAfraid("#" + afraid);
+    setAdmiration("#" + admiration);
+    setSurprise("#" + surprise);
+    setInterest("#" + interest);
+    setBoring("#" + boring);
+  }
+
+  const saveColorList = async () => {
+    const body = {
+      joy: joy?.replace("#", ""),
+      angry: angry?.replace("#", ""),
+      sad: sad?.replace("#", ""),
+      afraid: afraid?.replace("#", ""),
+      admiration: admiration?.replace("#", ""),
+      surprise: surprise?.replace("#", ""),
+      interest: interest?.replace("#", ""),
+      boring: boring?.replace("#", ""),
+    }
+    await API.post("/profile/update-color", body)
+  }
+
 
   return (
       <>
         <div className="profile-container">
           <div className="title">감정 선택</div>
           <div className="color-item">
-            <div className="title">joy</div>
-            <ColorPicker defaultValue={"#" + dummy.joy} value={joy} showText allowClear
-                         disabledAlpha onChange={(e) => {setJoy(e.toHexString())}}/>
+            <div className="title">기쁨</div>
+            {joy && <ColorPicker value={joy} showText allowClear
+                                 disabledAlpha onChange={(e) => {
+              setJoy(e.toHexString())
+            }}/>}
           </div>
           <div className="color-item">
-            <span className="title">angry</span>
-            <ColorPicker defaultValue={"#" + dummy.angry} value={angry} showText allowClear
-                         disabledAlpha onChange={(e) => {setAngry(e.toHexString())}}/>
+            <span className="title">화남</span>
+            {angry && <ColorPicker value={angry} showText allowClear
+                                   disabledAlpha onChange={(e) => {
+              setAngry(e.toHexString())
+            }}/>}
           </div>
           <div className="color-item">
-            <span className="title">sad</span>
-            <ColorPicker defaultValue={"#" + dummy.sad} value={sad} showText allowClear
-                         disabledAlpha onChange={(e) => {setSad(e.toHexString())}}/>
+            <span className="title">슬픔</span>
+            {sad && <ColorPicker value={sad} showText allowClear
+                                 disabledAlpha onChange={(e) => {
+              setSad(e.toHexString())
+            }}/>}
           </div>
           <div className="color-item">
-            <span className="title">afraid</span>
-            <ColorPicker defaultValue={"#" + dummy.afraid} value={afraid} showText allowClear
-                         disabledAlpha onChange={(e) => {setAfraid(e.toHexString())}}/>
+            <span className="title">두려움</span>
+            {afraid && <ColorPicker value={afraid} showText allowClear
+                                    disabledAlpha onChange={(e) => {
+              setAfraid(e.toHexString())
+            }}/>}
           </div>
           <div className="color-item">
-            <span className="title">admiration</span>
-            <ColorPicker defaultValue={"#" + dummy.admiration} value={admiration} showText
-                         allowClear disabledAlpha onChange={(e) => {setAdmiration(e.toHexString())}}/>
+            <span className="title">감탄</span>
+            {admiration && <ColorPicker value={admiration} showText
+                                        allowClear disabledAlpha onChange={(e) => {
+              setAdmiration(e.toHexString())
+            }}/>}
           </div>
           <div className="color-item">
-            <span className="title">surprise</span>
-            <ColorPicker defaultValue={"#" + dummy.surprise} value={surprise} showText allowClear
-                         disabledAlpha onChange={(e) => {setSurprise(e.toHexString())}}/>
+            <span className="title">놀람</span>
+            {surprise && <ColorPicker value={surprise} showText allowClear
+                                      disabledAlpha onChange={(e) => {
+              setSurprise(e.toHexString())
+            }}/>}
           </div>
           <div className="color-item">
-            <span className="title">interest</span>
-            <ColorPicker defaultValue={"#" + dummy.interest} value={interest} showText allowClear
-                         disabledAlpha onChange={(e) => {setInterest(e.toHexString())}}/>
+            <span className="title">호기심</span>
+            {interest && <ColorPicker value={interest} showText allowClear
+                                      disabledAlpha onChange={(e) => {
+              setInterest(e.toHexString())
+            }}/>}
           </div>
           <div className="color-item">
-            <span className="title">boring</span>
-            <ColorPicker defaultValue={"#" + dummy.boring} value={boring} showText allowClear
-                         disabledAlpha onChange={(e) => {setBoring(e.toHexString())}}/>
+            <span className="title">따분함</span>
+            {boring && <ColorPicker value={boring} showText allowClear
+                                    disabledAlpha onChange={(e) => {
+              setBoring(e.toHexString())
+            }}/>}
           </div>
+          <div className="color-item">
+            <Button type={"primary"}
+                    style={{width: "100px", fontSize: "15px", marginTop: "30px", marginRight: "10px"}}
+                    onClick={saveColorList}>저장</Button>
+          </div>
+
         </div>
 
 
