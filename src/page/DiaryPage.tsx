cf8d458606b7d2373ba27a4c25044559ce9cacec
jeongmin <jeongmin010324@gmail.com>
2024-11-09 19:19:09 +0900

    feat : 일기, 갤러리, 마이페이지 추가

---
+++ src/page/DiaryPage.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import "../asset/style/main.css"
+import TypingTextComponent from "../component/TypingTextComponent";
+import {Button} from "antd";
+import {useNavigate} from "react-router-dom";
+import {API} from "../util/api";
+// import {useTypingAnime} from "../component/TypingTextComponent";
+
+
+const DiaryPage = () => {
+  // const { animeFinishFlag : firstFlag, TypingTextDiv : FirstText } = useTypingAnime("First Line", 100)
+  // const { animeFinishFlag : secondFlag, TypingTextDiv : SecondText } = useTypingAnime("Second Line", 100, firstFlag)
+  // const { TypingTextDiv : ThirdText } = useTypingAnime("Third Line", 100, secondFlag)
+
+  const navigate  = useNavigate();
+
+  const handleStart = async () => {
+    try{
+      const params = new URLSearchParams();
+      params.append('message', 'test login');
+      await API.post(`/test/test-user?${params.toString()}`)
+      navigate("/diary");
+    }catch (e) {
+      navigate("/login");
+    }
+  }
+
+  return (<>
+    <div>
+      {/*<FirstText />*/}
+      {/*<SecondText />*/}
+      {/*<ThirdText />*/}
+      <div className="main-container">
+        <div className="title">
+          <TypingTextComponent text={'Diary Page'} />
+        </div>
+        <div className="start-button">
+          <Button color="default" variant="outlined" style={{width: "100px"}} onClick={handleStart}>
+            시작하기
+          </Button>
+        </div>
+      </div>
+
+    </div>
+  </>)
+}
+
+export default DiaryPage;
